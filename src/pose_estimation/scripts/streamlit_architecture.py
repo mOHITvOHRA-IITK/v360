c04d50c9760133becee15e5d894daebc4c0fb8d9
mOHITvOHRA-IITK <mvohra@iitk.ac.in>
2021-04-04 16:37:15 +0530

    client_interface

--- src/pose_estimation/scripts/streamlit_architecture.py
+++ src/pose_estimation/scripts/streamlit_architecture.py
@@ -1,104 +1,129 @@
+import os
 import cv2
 import streamlit as st
 from streamlit_class import *
-# from streamlit.ScriptRequestQueue import RerunData
-# from streamlit.ScriptRunner import RerunException
-# from streamlit.server.Server import Server
-# import streamlit.ReportThread as ReportThread
-
-
-# def rerun():
-#     """Rerun a Streamlit app from the top!"""
-#     widget_states = _get_widget_states()
-#     raise RerunException(RerunData(widget_states))
-
-
-height_feet = 0
-height_inch = 0
-
-count = 0
-
-height_feet, height_inch = get_height_feet_inch()
-store_height_feet(height_feet)
-store_height_inch(height_inch)
 
 
 st.title("Architecture")
 
-height = st.checkbox('Height')
+image_saved_path = './streamlit_images'
 
-if height:
-	height_feet, height_inch = get_height_feet_inch()
-	h_f_i = st.button('height_feet_increase')
-	if h_f_i:
-	    height_feet += 1
+live_feed, _, options = st.beta_columns([4, 1, 1])
 
+with options:
+	height = st.checkbox('Height')
+	save = st.checkbox('Save')
+	upload = st.checkbox('Upload')
 
+	feet_slider = st.empty()
+	inch_slider = st.empty()
 
-	h_f_d = st.button('height_feet_decrease')
-	if h_f_d:
-	    height_feet -= 1
-	    if height_feet < 0:
-	    	height_feet = 0
+	if height:
 
+		local_height_feet = feet_slider.slider('feets', min_value=0, max_value=7, value=None)
+		local_height_inch = inch_slider.slider('inches', min_value=0, max_value=12, value=None)
+		
+		if local_height_feet > 0:
+			store_height_feet(local_height_feet)
+			store_height_inch(local_height_inch)
 
-
-	h_i_i = st.button('height_inch_increase')
-	if h_i_i:
-	    height_inch += 1
-	    height_inch = height_inch % 12
-
-
-	h_i_d = st.button('height_inch_decrease')
-	if h_i_d:
-	    height_inch -= 1
-	    if height_inch < 0:
-	    	height_inch = 0
+		height_feet, height_inch = get_height_feet_inch()
+	else:
+		feet_slider.empty()
+		inch_slider.empty()
 
 
-	store_height_feet(height_feet)
-	store_height_inch(height_inch)
+	save_front_button = st.empty()
+	save_side_button = st.empty()
 
-placeholder2 = st.empty()
-key_count = 0
-string_data = str(height_feet) + ' feet, ' + str(height_inch) + ' inch'
-input = placeholder2.text_input('height', value=string_data, key=key_count)
+	if save:
+		s_f_i = save_front_button.button('save_front_image')
+		if s_f_i:
+			start_count_down()
+			save_front_side_images(True, None)
 
+			if os.path.isfile(image_saved_path + '/front.png'):
+				os.remove(image_saved_path + '/front.png')
+			if os.path.isfile(image_saved_path + '/side.png'):
+				os.remove(image_saved_path + '/side.png')
+			
+			
 
-save = st.checkbox('Save')
-
-if save:
-	s_f_i = st.button('save_front_image')
-	if s_f_i:
-		start_count_down()
-
-
-	s_s_i = st.button('save_side_image')
-	if s_s_i:
-		start_count_down()
-
-
-count_down = get_countdown()
-placeholder = st.empty()
-key_count = 0
-empty_string_data = '***********'
-input = placeholder.text_input('image countdown', value=empty_string_data, key=key_count)
-while(count_down > -1):
-
-	if save == False:
+		s_s_i = save_side_button.button('save_side_image')
+		if s_s_i:
+			start_count_down()
+	else:
 		stop_count_down()
-		break
-	key_count += 1
-	count_down = get_countdown()
-	string_data = 'saving image in ' +  str(count_down) + ' seconds'
-	input = placeholder.text_input('image countdown', value=string_data, key=key_count)
-	print (string_data, end="\r")
-
-	if count_down < 0:
-		input = placeholder.text_input('image countdown', value=empty_string_data, key=key_count)
+		save_front_button.empty()
+		save_side_button.empty()
 
 
 
 
+	file_uploader_button = st.empty()
+	if upload:
+		uploaded_files = file_uploader_button.file_uploader('upload saved images', accept_multiple_files=True)
+		for uploaded_file in uploaded_files:
+			st.image(uploaded_file, caption='Uploaded Image', use_column_width=True)
 
-	
+
+with live_feed:
+
+	feed = st.checkbox('Live Feed')
+
+	if feed:
+		initialize_webcam()
+	else:
+		stop_webcam()
+
+
+	FRAME_WINDOW = st.image([])
+	key_count = 0
+	height_text = st.empty()
+	save_text = st.empty()
+	image_save_status_text = st.empty()
+
+	while feed:
+		# visualize_images()
+		frame = get_current_feed()
+		frame = cv2.flip(frame, 1) 
+		frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+		FRAME_WINDOW.image(frame)
+
+		key_count += 1
+
+		if height:
+			string_data = str(height_feet) + ' feet, ' + str(height_inch) + ' inch'
+			input = height_text.text_input('height', value=string_data, key=key_count)
+
+		if save:
+			
+			if check_both_images_saved():
+				string_data = os.getcwd() + image_saved_path
+				input = image_save_status_text.text_input('image save status', value=string_data, key=key_count)
+			else:
+				string_data = 'Need To save images'
+				input = image_save_status_text.text_input('image save status', value=string_data, key=key_count)
+
+				count_down = get_countdown()
+				empty_string_data = '***********'
+				string_data = 'saving image in ' +  str(count_down) + ' seconds'
+				if count_down > -1:
+					input = save_text.text_input('image countdown', value=string_data, key=key_count)
+					if count_down == 0:
+						frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+						if os.path.isfile(image_saved_path + '/front.png') == False:
+							cv2.imwrite(image_saved_path + '/front.png', frame)
+
+						elif os.path.isfile(image_saved_path + '/side.png') == False:
+							cv2.imwrite(image_saved_path + '/side.png', frame)
+
+						save_front_side_images(False, frame)
+				else:
+					input = save_text.text_input('image countdown', value=empty_string_data, key=key_count)
+
+
+		
+			
+		
+
--- src/pose_estimation/scripts/streamlit_class.py
+++ src/pose_estimation/scripts/streamlit_class.py
@@ -1,5 +1,10 @@
+import cv2
 import time
 import numpy as np
+
+global camera_object_start, camera_object
+camera_object_start = False
+
 
 global height_feet, height_inch
 height_feet = 0
@@ -9,6 +14,12 @@ global start_count, timer_start_flag, start_time
 start_count = False
 timer_start_flag = False
 start_time = 0
+
+
+global front_image, front_image_flag, side_image, side_image_flag
+front_image_flag = False
+side_image_flag = False
+
 
 countdown = 5
 
@@ -51,18 +62,96 @@ def get_countdown():
 		start_time = time.time()
 
 	count_down = countdown - np.int( time.time() - start_time)
-	# print ('countdown', countdown)
-	# print ('time.time()', time.time())
-	# print ('start_time', start_time)
-	# print ('count_down', count_down)
-	# print ('start_count', start_count)
-	# print ()
 
 	if count_down <0:
 		timer_start_flag = False
 		start_count = False
 
+	if count_down == 0 and timer_start_flag:
+		start_count = False
+		timer_start_flag = False
+		return 0
+
 	if start_count == True:
 		return count_down
 	else:
 		return -1
+
+
+
+
+def initialize_webcam():
+	global camera_object_start, camera_object
+
+	if camera_object_start == False:
+		camera_object_start = True
+		camera_object = cv2.VideoCapture(0)
+
+
+
+def get_current_feed():
+	global camera_object_start, camera_object
+
+	if camera_object_start == True:
+		_, frame = camera_object.read()
+		return frame
+	else:
+		return None
+
+
+
+def stop_webcam():
+	global camera_object_start, camera_object
+
+	initialize_webcam()
+
+	camera_object_start = False
+	camera_object.release()
+
+
+
+
+def save_front_side_images(reset, frame):
+	global front_image, front_image_flag, side_image, side_image_flag
+
+	if reset:
+		front_image_flag = False
+		side_image_flag = False
+
+	else:
+
+		if front_image_flag == False:
+			front_image_flag = True
+			front_image = frame
+
+		elif side_image_flag == False:
+			side_image_flag = True
+			side_image = frame
+
+
+
+def visualize_images():
+	global front_image, front_image_flag, side_image, side_image_flag
+
+	if front_image_flag:
+		cv2.imshow('front_image', front_image)
+		cv2.waitKey(1)
+	
+		
+	if side_image_flag:
+		cv2.imshow('side_image', side_image)
+		cv2.waitKey(1)
+	
+		
+
+
+def check_both_images_saved():
+	global front_image_flag, side_image_flag
+	if front_image_flag and side_image_flag:
+		return True
+	else:
+		return False
+
+
+
+
